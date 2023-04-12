5d0efae4cce98a2bfd1c85fa966c5c6ec5104aa5
Andres Casasfranco <46802957+afcasasfranco@users.noreply.github.com>
2023-04-12 17:39:28 -0500

    Update CloudFlareIPUpdate.py

--- CloudFlareIPUpdate.py
+++ CloudFlareIPUpdate.py
@@ -2,15 +2,37 @@ import requests, configparser, time, json, logging, datetime
 import mysql.connector
 # Reading the keys from the cfauth.ini file
 config = configparser.ConfigParser()
-config.read('cfauth.ini')
+config.read('/home/afcasasfranco/Docker/Script/CloudFlareIP/cfauth.ini')
 
 #All your DNS Zonez here & Token
 zone = json.loads(config.get('zone', 'zone'))
-block_update = json.loads(config.get('no_update', 'no_update'))
+block_no_update = json.loads(config.get('no_update', 'no_update'))
 bearer_token = config.get('tokens', 'bearer_token')
-zone_count = len(zone)
+zone_count = range(len(zone))
 # Get the time of the IP change
 now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+
+#MYSQL Config
+config = {
+  'host':'0.0.0.0',
+  'user':'root',
+  'password':'153190',
+  'database':'cloudflare'
+}
+
+#MySQL connect
+db = mysql.connector.connect(**config)
+cursor = db.cursor()
+
+#Create Tables
+try:
+    cursor.execute("CREATE TABLE DNS (zoneid VARCHAR(255), register_id VARCHAR(255), name VARCHAR(255) PRIMARY KEY, ipchange BOOLEAN DEFAULT True, content VARCHAR(18))")
+    cursor.execute("CREATE TABLE IP (id INT PRIMARY KEY, ip VARCHAR(20)")
+    cursor.close()
+except:
+    cursor.close()
     
 # Setting up the logger (a file where it records all IP changes)
 logging.basicConfig(level=logging.INFO, filename='ipchanges.log', format='%(levelname)s :: %(message)s')
@@ -26,6 +48,7 @@ def find_cf(i):
     zone_id = zone[i]
     zone_result = requests.get(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records?page=1&per_page=20&order=type&direction=asc", headers=headers)
     zone_result = zone_result.json()['result']
+    #print (zone_result)
     return zone_result
     
 #Server IP
@@ -33,14 +56,14 @@ def my_ip():
     my_ip = requests.get("https://api.ipify.org?format=json")
     my_ip = my_ip.json()['ip']
     return my_ip
-    print (my_ip)
+    
 
 #Compare between server ip and cloudflare ip
 def compare_ips():
     real_ip = my_ip()
     cf_ip = find_cf(0)[0]['content']
+    print (cf_ip)
     if real_ip == cf_ip:
-        #You can change the waiting time its 600 seg = 10 minutes
         time.sleep(600)
         main()
     else:
@@ -49,28 +72,25 @@ def compare_ips():
 
 
 def update_ip():
-    i = 0
-    while i < zone_count:
+    for i in zone_count:
         zone_id = zone[i]
         records = range(len(find_cf(i)))
-        block = range(len(block_update))
+        no_update = range(len(block_no_update))
         for x in records:
+            print(i, x)
             if find_cf(i)[x]['type'] == 'A':
-                for n in block:
-                    if find_cf(i)[x]['name'] != block_update[n]:
-                        payload = {'content': my_ip()}
-                        record_id = find_cf(i)[x]['id']
-                        # Change the IP using a PUT request\
-                        requests.patch(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}", headers=headers, data=json.dumps(payload))
-                    else:
-                      pass
-            else:
-                pass
-    
-        i = i+1
-    compare_ips()
+                if find_cf(i)[x]['name'] in block_no_update:
+                    pass
+                else:
+                    print (find_cf(i)[x]['name'])
+                    payload = {'content': my_ip()}
+                    record_id = find_cf(i)[x]['id']
+                    # Change the IP using a PUT request\
+                    requests.patch(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}", headers=headers, data=json.dumps(payload))   
+compare_ips()
     
 def main():
     compare_ips()
+
 
 main()
